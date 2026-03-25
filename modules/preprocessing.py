841be0fa1898af17f0c01ad7b9763bce677f0458
phngan05 <phngan100705@gmail.com>
2026-03-25 16:55:16 +0700

    update preprocessing.py

--- modules/preprocessing.py
+++ modules/preprocessing.py
@@ -1,9 +1,9 @@
 import spacy
 
-def lemma_data(text):
+def lemma_data(df, column):
     nlp = spacy.load("en_core_web_sm", disable=['ner', 'parser'])
     processed_texts = []
-    for doc in nlp.pipe(df.astype(str), batch_size=500, n_process=-1):
+    for doc in nlp.pipe(df[column].astype(str), batch_size=500, n_process=-1):
         tokens = [t.lemma_ for t in doc if t.pos_ in ['NOUN', 'ADJ', 'VERB']]
         processed_texts.append(" ".join(tokens))
     return processed_texts
